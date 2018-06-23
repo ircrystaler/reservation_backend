d73c1880f3056babfda309bded6802f98b13d995
Behdad Ahmadi <sbmahmadi@gmail.com>
2018-06-23 16:21:10 +0430

    Base Code

---
+++ reservation_system/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reservation_system.Controllers
+{
+    [Route("api/user")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult getUser(int id)
+        {
+            return new JsonResult(id);
+        }
+    }
+}
